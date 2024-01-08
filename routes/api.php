b749b4edfbeeb8ff0dd314ecc3e4326765ef8de6
Blaž Štefin <blaz.stefin@gmail.com>
2024-01-08 12:48:33 +0100

    fix: API middleware configuration

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['namespace' => 'API', 'middleware' => ['guest', 'cors']], function () {
+Route::group(['namespace' => 'API', 'middleware' => ['guest']], function () {
     Route::post("login", [AuthController::class, "login"]);
     Route::post("register", [AuthController::class, "register"]);
 });
 
-Route::group(['namespace' => 'API', 'middleware' => ['auth:sanctum', 'cors']], function () {
+Route::group(['namespace' => 'API', 'middleware' => ['auth:sanctum']], function () {
     Route::post("logout", [AuthController::class, "logout"]);
 
     Route::get('feed', [PostController::class, 'feed'])->name('feed');
