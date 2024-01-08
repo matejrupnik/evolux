49cfed40df59ff9f9064259e29dcb5fcf0462aca
Blaž Štefin <blaz.stefin@gmail.com>
2024-01-08 12:54:06 +0100

    fix: api imports

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\Auth\AuthController;
+use App\Http\Controllers\API\PostController;
+use App\Http\Controllers\API\UserController;
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
