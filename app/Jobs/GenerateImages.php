6a0f2b59c7b740027f90b075cff3fc4e08c6aa10
matejrupnik <matej.rupnik02@gmail.com>
2024-01-14 12:05:28 +0100

    feat: everything bagel

---
+++ app/Jobs/GenerateImages.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Keyword;
+use App\Models\Media;
+use App\Models\Post;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class GenerateImages implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $keywords;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($keywords)
+    {
+        $this->keywords = $keywords;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        foreach ($this->keywords as $keyword) {
+            $url = "http://46.164.49.18:7860";
+            $file = "words_alpha.txt";
+            $file_arr = file(public_path().'/'.$file);
+            $num_lines = count($file_arr);
+            $last_arr_index = $num_lines - 1;
+            $rand_index = rand(0, $last_arr_index);
+            $word = trim($file_arr[$rand_index]);
+
+            $payload = [
+                "prompt" => 'realistic '.$keyword->keyword . ' ' . $word,
+                "steps" => 10,
+                "height" => 1024,
+                "width" => 1024
+            ];
+
+            $response = Http::post("$url/sdapi/v1/txt2img", $payload);
+            $responseData = $response->json();
+
+            $base64Image = $responseData['images'][0];
+            $imageData = base64_decode($base64Image);
+
+            $filename = 'output_' . Str::random(8) . '.png';
+            Storage::put("public/images/$filename", $imageData);
+
+            $media = Media::factory()->create(['file_name' => $filename]);
+            $post = Post::factory()->create(['media_id' => $media->id]);
+            $newKeyword = Keyword::create(['keyword' => $word]);
+            $post->keywords()->saveMany([$keyword, $newKeyword]);
+
+            Log::info('Image generated.');
+        }
+    }
+}
