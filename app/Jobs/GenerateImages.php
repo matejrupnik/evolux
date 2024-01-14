545003e6a961e6a939489dc979e8fae365f8fc86
matejrupnik <matej.rupnik02@gmail.com>
2024-01-14 12:13:11 +0100

    feat: everything bagel

--- app/Jobs/GenerateImages.php
+++ app/Jobs/GenerateImages.php
@@ -37,36 +37,7 @@ class GenerateImages implements ShouldQueue
     public function handle(): void
     {
         foreach ($this->keywords as $keyword) {
-            $url = "http://46.164.49.18:7860";
-            $file = "words_alpha.txt";
-            $file_arr = file(public_path().'/'.$file);
-            $num_lines = count($file_arr);
-            $last_arr_index = $num_lines - 1;
-            $rand_index = rand(0, $last_arr_index);
-            $word = trim($file_arr[$rand_index]);
-
-            $payload = [
-                "prompt" => 'realistic '.$keyword->keyword . ' ' . $word,
-                "steps" => 10,
-                "height" => 1024,
-                "width" => 1024
-            ];
-
-            $response = Http::post("$url/sdapi/v1/txt2img", $payload);
-            $responseData = $response->json();
-
-            $base64Image = $responseData['images'][0];
-            $imageData = base64_decode($base64Image);
-
-            $filename = 'output_' . Str::random(8) . '.png';
-            Storage::put("public/images/$filename", $imageData);
-
-            $media = Media::factory()->create(['file_name' => $filename]);
-            $post = Post::factory()->create(['media_id' => $media->id]);
-            $newKeyword = Keyword::create(['keyword' => $word]);
-            $post->keywords()->saveMany([$keyword, $newKeyword]);
-
-            Log::info('Image generated.');
+            GenerateImage::dispatch($keyword);
         }
     }
 }
